15f8cd443b1f75917177516cc5c7ea2b97d8f006
mamomeri <mamomeri@espol.edu.ec>
2024-07-10 10:18:41 -0500

    actualizar2

--- server.js
+++ server.js
@@ -5,6 +5,12 @@
  const admin = require('firebase-admin');
  const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_API)
 
+  /* Referencia al módulo swagger-ui-express */
+  const swaggerUi = require('swagger-ui-express')
+
+  /* Referencia al archivo con la descripción */
+  const swaggerFile = require('./swagger_output.json')
+
  admin.initializeApp({
    credential: admin.credential.cert(serviceAccount)
  });
@@ -19,3 +25,8 @@
  app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`);
  });
+
+  /* Ruta Base -> Documentación */
+  app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+  app.use('/api', require('./routes/api'));
